a575b34496edb28d4912393bd0d697deb9975312
darkgsl <54315640+darkgsl@users.noreply.github.com>
2021-05-15 21:52:25 +0300

    feat: fix exercise 3

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,14 +3,42 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-  
-  if (self.count != 0) {
-    NSArray *sortedArray = [self sortedArrayUsingSelector:@selector(compare:)];
-    return sortedArray[0];
     
-  } else {
-    return nil;
-  }
+    if (self.count == 0) {
+        return  nil;
+    }
+    
+    NSUInteger low = 0;
+    NSUInteger high = self.count - 1;
+    NSUInteger medium = (low + high) / 2;
+    bool isFinish = false;
+    
+    NSUInteger resultIndex = 0;
+    NSInteger valueMediumIndex;
+    NSInteger valueLowIndex;
+    NSInteger valueHighIndex;
+    
+    while (isFinish != true) {
+        valueMediumIndex = [[self objectAtIndex:medium] integerValue];
+        valueLowIndex = [[self objectAtIndex:low] integerValue];
+        valueHighIndex = [[self objectAtIndex:high] integerValue];
+
+        if (low == medium || high == medium) {
+            resultIndex =  valueLowIndex < valueHighIndex ? low : high;
+            return self[resultIndex];
+        }
+        
+        if (valueMediumIndex < valueHighIndex) {
+            high = medium;
+        } else if (valueMediumIndex > valueHighIndex) {
+            low = medium;
+        } else {
+            resultIndex = medium;
+            isFinish = true;
+        }
+        medium = (low + high) / 2;
+    }
+    return self[resultIndex];
 }
 
 @end
